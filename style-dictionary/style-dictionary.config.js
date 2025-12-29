71f255eff2481239e3edf0194f468b4a50b2baac
asahara.chihiro <asahara.chihiro.23e@jp.nssol.nipponsteel.com>
2025-12-29 11:40:22 +0900

    SD出力をprimitives/semanticで分離

--- style-dictionary/style-dictionary.config.js
+++ style-dictionary/style-dictionary.config.js
@@ -7,8 +7,14 @@ export default {
       buildPath: "build/web/",
       files: [
         {
-          destination: "tokens.json",
+          destination: "primitives.json",
           format: "json",
+          filter: (token) => token.path[0] === "primitives",
+        },
+        {
+          destination: "semantic.json",
+          format: "json",
+          filter: (token) => token.path[0] === "semantic",
         },
       ],
     },
